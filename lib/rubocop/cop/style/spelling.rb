a0b8a3be3c25aaf6868f33667c7ce303b47b5659
Andrew Grimm <andrew.j.grimm@gmail.com>
2014-06-03 18:19:21 +1000

    Check spelling of local variables

--- lib/rubocop/cop/style/spelling.rb
+++ lib/rubocop/cop/style/spelling.rb
@@ -5,6 +5,20 @@ module Rubocop
     module Style
       # Checks spelling
       class Spelling < Cop
+        MSG = 'Incorrectly spelled word'
+
+        def on_lvasgn(node)
+          lv_symbol = node.children.first
+          lv_name = lv_symbol.to_s
+          # ASSUMPTION: All local variables are split into words by underscores.
+          words = lv_name.split('_')
+          words.each do |word|
+            next if word.empty?
+            next if known_words.include?(word)
+            add_offense(node, :expression)
+          end
+        end
+
         def known_words
           @known_words ||= determine_known_words
         end
--- spec/rubocop/cop/style/spelling_spec.rb
+++ spec/rubocop/cop/style/spelling_spec.rb
@@ -5,6 +5,11 @@ require 'spec_helper'
 describe Rubocop::Cop::Style::Spelling do
   subject(:cop) { described_class.new }
 
+  it 'registers an offense for an incorrectly spelled local variable' do
+    inspect_source(cop, 'offence = Offense.new')
+    expect(cop.offenses.size).to eq(1)
+  end
+
   it 'does not register an offense for a correctly spelled local variable' do
     inspect_source(cop, 'offense = Offense.new')
     expect(cop.offenses).to be_empty
