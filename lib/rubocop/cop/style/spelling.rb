54bb9ce43d8afad7023c540d7bdb8008f65e7b29
Andrew Grimm <andrew.j.grimm@gmail.com>
2014-06-04 23:18:01 +1000

    Add comments about work in progress

--- lib/rubocop/cop/style/spelling.rb
+++ lib/rubocop/cop/style/spelling.rb
@@ -3,9 +3,39 @@
 module Rubocop
   module Cop
     module Style
+      # TODO: Add automatic generation of the dictionary file.
+      # TODO: Add handling of camelCase variables.
+      # TODO: Add handling of more syntax, such as on_def.
+      # TODO: Add more informative message for incorrectly spelled words.
+      # TODO: Reduce repetition in code.
+      # TODO: Ensure this works on other projects.
       # Checks spelling
       class Spelling < Cop
         MSG = 'Incorrectly spelled word'
+
+        # rubocop:disable Style/LineLength
+        # Listed as in the same order in Parser::AST::Processor
+
+        # Descriptions in https://github.com/whitequark/parser/blob/master/doc/AST_FORMAT.md
+
+        # dstr means "String with interpolation". Strings are content, and content may or may not be correctly spelled.
+        # dsym means "Symbol with interpolation". Symbols are content, and content may or may not be correctly spelled.
+        # regexp means "Regexp". Regular expressions do not obey spelling.
+        # xstr means "Execute-string". System commands do not obey spelling.
+        # splat means "Splat". It always contains a lvar, so leave evaluation of spelling to on_lvar.
+        # array means "Array". It is always made up of other elements, so leave evaluation of spelling to them.
+        # pair is always made up of other elements, so leave evaluation of spelling to them.
+        # hash is always made up of other elements, so leave evaluation of spelling to them.
+        # irange is always made up of other elements, so leave evaluation of spelling to them.
+        # erange is always made up of other elements, so leave evaluation of spelling to them.
+        # var appears to be a parent type of lvar, ivar, gvar, cvar, back_ref, nth_ref. Defer for now.
+        # lvar, ivar, gvar, cvar, back_ref, nth_ref presumably refer to the use of existing variables. Try to examine the creation, not use, of variables.
+        # and-asgn is always made up of other elements, so leave evaluation of spelling to them.
+        # or-asgn is always made up of other elements, so leave evaluation of spelling to them.
+        # op-asgn is always made up of other elements, so leave evaluation of spelling to them.
+        # mlhs is always made up of other elements, so leave evaluation of spelling to them.
+        # masgn is always made up of other elements, so leave evaluation of spelling to them.
+        # const presumably refers to the use of existing constants. Try to examine the creation, not use, of constants.
 
         def on_lvasgn(node)
           lv_symbol = node.children.first
