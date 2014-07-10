c4261df8cf7d398648c036b3becc0d9ce848adfa
Andrew Grimm <andrew.j.grimm@gmail.com>
2014-07-10 21:28:09 +1000

    DRY up code

--- lib/rubocop/cop/style/spelling.rb
+++ lib/rubocop/cop/style/spelling.rb
@@ -17,47 +17,11 @@ module Rubocop
 
         # Descriptions in https://github.com/whitequark/parser/blob/master/doc/AST_FORMAT.md
 
-        def on_lvasgn(node)
-          lv_symbol = node.children.first
-          lv_name = lv_symbol.to_s
+        def on_vasgn(node)
+          v_symbol = node.children.first
+          v_name = v_symbol.to_s.gsub(/[$@]/, '')
           # ASSUMPTION: All variables are split into words by underscores.
-          words = lv_name.split('_')
-          words.each do |word|
-            next if word.empty?
-            next if known_words.include?(word)
-            add_offense(node, :expression)
-          end
-        end
-
-        def on_ivasgn(node)
-          iv_symbol = node.children.first
-          iv_name = iv_symbol.to_s[1..-1]
-          # ASSUMPTION: All variables are split into words by underscores.
-          words = iv_name.split('_')
-          words.each do |word|
-            next if word.empty?
-            next if known_words.include?(word)
-            add_offense(node, :expression)
-          end
-        end
-
-        def on_gvasgn(node)
-          gv_symbol = node.children.first
-          gv_name = gv_symbol.to_s[1..-1]
-          # ASSUMPTION: All variables are split into words by underscores.
-          words = gv_name.split('_')
-          words.each do |word|
-            next if word.empty?
-            next if known_words.include?(word)
-            add_offense(node, :expression)
-          end
-        end
-
-        def on_cvasgn(node)
-          cv_symbol = node.children.first
-          cv_name = cv_symbol.to_s[2..-1]
-          # ASSUMPTION: All variables are split into words by underscores.
-          words = cv_name.split('_')
+          words = v_name.split('_')
           words.each do |word|
             next if word.empty?
             next if known_words.include?(word)
